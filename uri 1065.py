95a5c3593152472fc7b01130bd939083835d6cef
Jessica Monteiro <101065281+monteirojess@users.noreply.github.com>
2022-05-11 22:33:32 -0300

    Update uri 1065.py

--- uri 1065.py
+++ uri 1065.py
@@ -1,3 +1,9 @@
+# Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+#Entrada
+#O arquivo de entrada contém 5 valores inteiros quaisquer.
+#Saída
+#Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+
 cont = number = 0
 for number in range (5):
     number = float(input())
